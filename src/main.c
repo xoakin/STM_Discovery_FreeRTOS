9f1adda05f7180a004218b69f02ca2c8c5492dca
Joaquin Juan Toledo <joaquin.j.toledo@gmail.com>
2016-10-30 20:37:45 +0100

    [DEV] Modify app to use FreeRTOS timers instead

--- src/main.c
+++ src/main.c
@@ -15,39 +15,64 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "timers.h"
+#include "stm32f4xx_it.h"
 
 
 
-void ToggleLED4_task(void * pvParameters);
+void ToggleLED4_callback(TimerHandle_t xTimer);
+void ToggleLED5_callback(TimerHandle_t xTimer);
+void ToggleLeds(void *pvParameters);
+
+
 int main(void)
 {
 	SystemInit();
-	SystemCoreClockUpdate();
-//	HAL_Init();
+	SysTick_Handler();
 
 	BSP_LED_Init(LED4);
 	BSP_LED_Init(LED5);
 
 	xTaskCreate(
-		ToggleLED4_task,
-		"Task1",
+		ToggleLeds,
+		"Toggle",
 		configMINIMAL_STACK_SIZE,
 		NULL,
 		tskIDLE_PRIORITY + 2,
 		NULL);
-
 
 	vTaskStartScheduler();
 
 	for(;;);
 }
 
-
-void ToggleLED4_task(void * pvParameters)
+void ToggleLeds(void *pvParameters)
 {
+	xTimerHandle timer1, timer2;
+
+	timer1 = xTimerCreate("Timer1",
+			500/portTICK_PERIOD_MS,
+			pdTRUE, NULL, ToggleLED4_callback);
+
+	timer2 = xTimerCreate("Timer2",
+			1000/portTICK_PERIOD_MS,
+			pdTRUE, NULL, ToggleLED5_callback);
+
+	xTimerStart(timer1, 0);
+	xTimerStart(timer2, 0);
+
 	while(1)
 	{
-		BSP_LED_Toggle(LED4);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
+
 	}
+
 }
+void ToggleLED4_callback(TimerHandle_t xTimer)
+{
+	BSP_LED_Toggle(LED4);
+}
+
+void ToggleLED5_callback(TimerHandle_t xTimer)
+{
+	BSP_LED_Toggle(LED5);
+}
+
